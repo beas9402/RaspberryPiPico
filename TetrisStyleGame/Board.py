ff7f3b2cd7689e511e30c27648176b7cc43c8610
Rob Beasley <rob@beasleyville.com>
2024-09-03 09:13:08 -0700

    Remove display logic from the board.  Refactor and rename

--- TetrisStyleGame/Board.py
+++ TetrisStyleGame/Board.py
@@ -1,138 +1,112 @@
-#include Colors.py
-
+# define EMPTY as zero - when drawing cells be sure to check for EMPTY cells
 EMPTY = 0
-
+# each cell in the grid starts as empty
+# and droppable
 class cell:
-    state    = EMPTY
+    color = EMPTY
     can_drop = True
-    def __init__(self):
-        return
-    def __repr__(self):
-        return '{}{}'.format(' ' if self.state == EMPTY else self.state,
-                                   '↓' if self.can_drop else ' ')
+
     def clear(self):
-        self.state = EMPTY
+        self.color = EMPTY
         self.can_drop = True
 
-class board:
-    def __init__(self, x, y, rows=20, columns=10, brick=10):
-        self.origin = (x, y)
-        self.size   = (columns, rows)
-        self.brick  = brick
-        self.center = brick-2
-        self.stride = brick + 1
-        self.wide   = (self.stride * columns) + 1
-        self.high   = (self.stride * rows)    + 1
-        self.score  = 0
-        self.delete = []
-        #self.empty  = []
-        self.rows   = [[cell() for c in range(columns)]
-                                    for r in range(rows)]
+    def __repr__(self):
+        return "{}{}".format(
+            " " if self.color == EMPTY else self.color, "↓" if self.can_drop else " "
+        )
 
-        
-    def clear_delete(self):
-        delete_count = len(self.delete)
-        self.score  += (100 * delete_count)
-        top          =  self.size[0] - 1
-        droppable    = False
+
+# default board is 20 rows and 10 columns
+class board:
+    def __init__(self, rows=20, columns=10):
+        self.size = (columns, rows)
+        self.score = 0
+        self.rows = [[cell() for c in range(columns)] for r in range(rows)]
+
+    def _find_full_rows(self):
+        full_rows = []
+        for r, row in enumerate(self.rows):
+            count = 0
+            for c, cell in enumerate(row):
+                if cell.color != EMPTY:
+                    count += 1
+
+            if count == len(row):
+                full_rows.append(r)
+
+        return full_rows
+
+    def delete_full_rows(self):
+        rows_to_delete = self._find_full_rows()
+
+        if not rows_to_delete:
+            return False
+
+        count = len(rows_to_delete)
+
+        self.score += 100 * count * count
+        top = self.size[0] - 1
+        droppable = False
+
         for r in range(self.size[1]):
             for c in range(self.size[0]):
-                #print(r, c, r in self.delete)
-                if r in self.delete:
+                if r in rows_to_delete:
                     droppable = True
-                    self.score += (self.rows[r][c].state * delete_count)
+                    self.score += self.rows[r][c].color * count
                     self.rows[r][c].clear()
                 elif droppable:
                     self.rows[r][c].can_drop = droppable
-        self.delete = []
-        
-    def compact_bricks(self):
-        first = 0 # self.empty.pop(0)
-        
+        print("Rows deleted", len(rows_to_delete))
+        self._print()
+        return True
+
+    def drop(self):
+        dropped = 0
         for r in range(self.size[1]):
             for c in range(self.size[0]):
                 current = self.rows[r][c]
                 if r == 0:
                     current.can_drop = False
                 else:
-                    below = self.rows[r-1][c]
-                    if current.can_drop and below.state == EMPTY:
-                        below.state = current.state
+                    below = self.rows[r - 1][c]
+                    if (
+                        current.can_drop
+                        and current.color != EMPTY
+                        and below.color == EMPTY
+                    ):
+                        below.color = current.color
                         below.can_drop = True
                         current.clear()
+                        dropped += 1
                     else:
                         current.can_drop == False
-        #self.empty = []
+        if dropped > 0:
+            print("Dropped", dropped)
+            self._print()
+        return dropped > 0
 
-    def draw(self, board, ):
-        print('***************** DRAW *************')
-        x, y = self.origin
-        canvas.set_pen(BLACK)
-        canvas.clear()
-        canvas.set_pen(WHITE)
-        canvas.rectangle(x, y, self.wide, self.high)
-
-        for r, row in enumerate(self.rows):
-            drawcount = 0
-            for c, cell in enumerate(row):
-                if cell.state != EMPTY:
-                    cell_x = (c * self.stride) + x + 1
-                    cell_y = ((self.size[1]-1 - r) * self.stride) + y + 1
-                    canvas.set_pen(BLACK)
-                    canvas.rectangle(cell_x, cell_y, self.brick, self.brick)
-                    canvas.set_pen(cell.state)
-                    canvas.rectangle(cell_x + 1, cell_y + 1, self.center, self.center)
-                    drawcount += 1
-                    
-            if drawcount == len(row):
-                self.delete.append(r)
-
-
-        canvas.update()
-        self._print()   
-    def _find_full_rows(self):
-        for r, row in enumerate(self.rows):
-            count = 0
-            for c, cell in enumerate(row):
-                if cell.state != EMPTY:
-                    count += 1
-                    
-            if count == len(row):
-                self.delete.append(r)
-  
- 
     def _print(self, all=False):
-        if all:
-            print('origin :', self.origin)
-            print('size   :', self.size)
-            print('brick  :', self.brick)
-            print('center :', self.center)
-            print('stride :', self.stride)
-            print('wide   :', self.wide )
-            print('high   :', self.high)
-        print('rows')
+        print("size   :", self.size)
+        print("rows")
         for i in reversed(range(self.size[1])):
-            print('{:>6} :'.format(i), self.rows[i])
-        
-        print('score  :', self.score )
-        print('delete :', self.delete )
+            print("{:>6} :".format(i), self.rows[i])
+        print("score  :", self.score)
 
     def _load_random(self):
         for r in range(self.size[1]):
             for c in range(self.size[0]):
-                self.rows[r][c].state = random.randint(0, 6)
+                self.rows[r][c].color = random.randint(0, 6)
                 self.rows[r][c].can_drop = False
- 
- 
- 
+
+'''
 ####################### TESTING ############################
-import time
 import random
 
-
-b = board(x=11, y=0, rows=10)
+b = board(rows=10)
 b._print()
 b._load_random()
 b._print()
-b._find_full_rows()
-b._print()
+done = False
+while not done:
+    done = not b.drop() and not b.delete_full_rows()
+############################################################'''
