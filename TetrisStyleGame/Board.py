d46051e18d351bfab36f9a972f16443b88b44e8a
Rob Beasley <rob@beasleyville.com>
2024-09-03 11:43:47 -0700

    fix a bug where can_drop was not being updated properly

--- TetrisStyleGame/Board.py
+++ TetrisStyleGame/Board.py
@@ -3,31 +3,30 @@ EMPTY = 0
 # each cell in the grid starts as empty
 # and droppable
 class cell:
-    color = EMPTY
+    color    = EMPTY
     can_drop = True
 
     def clear(self):
-        self.color = EMPTY
+        self.color    = EMPTY
         self.can_drop = True
 
     def __repr__(self):
-        return "{}{}".format(
-            " " if self.color == EMPTY else self.color, "↓" if self.can_drop else " "
-        )
+        return "{}{}".format(" " if self.color == EMPTY else self.color, 
+                             "↓" if self.can_drop else " ")
 
 
 # default board is 20 rows and 10 columns
 class board:
     def __init__(self, rows=20, columns=10):
-        self.size = (columns, rows)
+        self.size  = (columns, rows)
         self.score = 0
-        self.rows = [[cell() for c in range(columns)] for r in range(rows)]
+        self.rows  = [[cell() for c in range(columns)] for r in range(rows)]
 
     def _find_full_rows(self):
         full_rows = []
         for r, row in enumerate(self.rows):
             count = 0
-            for c, cell in enumerate(row):
+            for cell in row:
                 if cell.color != EMPTY:
                     count += 1
 
@@ -43,21 +42,19 @@ class board:
             return False
 
         count = len(rows_to_delete)
-
         self.score += 100 * count * count
-        top = self.size[0] - 1
         droppable = False
 
         for r in range(self.size[1]):
             for c in range(self.size[0]):
                 if r in rows_to_delete:
-                    droppable = True
+                    droppable   = True
                     self.score += self.rows[r][c].color * count
                     self.rows[r][c].clear()
                 elif droppable:
                     self.rows[r][c].can_drop = droppable
-        print("Rows deleted", len(rows_to_delete))
-        self._print()
+
+        self._print("Rows deleted {}".format(len(rows_to_delete)), DEBUG)
         return True
 
     def drop(self):
@@ -69,44 +66,49 @@ class board:
                     current.can_drop = False
                 else:
                     below = self.rows[r - 1][c]
-                    if (
-                        current.can_drop
-                        and current.color != EMPTY
-                        and below.color == EMPTY
-                    ):
+                    if (current.can_drop and current.color != EMPTY
+                                         and below.color   == EMPTY):
                         below.color = current.color
                         below.can_drop = True
                         current.clear()
                         dropped += 1
                     else:
-                        current.can_drop == False
+                        current.can_drop = False
         if dropped > 0:
-            print("Dropped", dropped)
-            self._print()
+            self._print("Dropped", DEBUG)
         return dropped > 0
 
-    def _print(self, all=False):
-        print("size   :", self.size)
-        print("rows")
-        for i in reversed(range(self.size[1])):
-            print("{:>6} :".format(i), self.rows[i])
-        print("score  :", self.score)
+    def _print(self, message, is_debug):
+        if is_debug:
+            print(message)
+            print("size   :", self.size)
+            print("rows")
+            for i in reversed(range(self.size[1])):
+                print("{:>6} :".format(i), self.rows[i])
+            print("score  :", self.score)
 
+
+#'''
+####################### TESTING ############################
     def _load_random(self):
         for r in range(self.size[1]):
             for c in range(self.size[0]):
                 self.rows[r][c].color = random.randint(0, 6)
                 self.rows[r][c].can_drop = False
 
-'''
-####################### TESTING ############################
 import random
 
+DEBUG = True
+
+
 b = board(rows=10)
-b._print()
+b._print('Empty Board', DEBUG)
 b._load_random()
-b._print()
+b._print('Load Random', DEBUG)
 done = False
 while not done:
     done = not b.drop() and not b.delete_full_rows()
+    
+    
+b.drop()    
 ############################################################'''
